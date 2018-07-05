df83a92bb23d0124e1a77d1977776b5752c36012
Jordy Mendoza <jordyamc@hotmail.com>
2018-07-05 13:51:48 -0500

    -Nombres arreglados

--- src/main/java/mx/com/nitrostudio/animechannel/models/entities/servers/hosts/OkruServer.kt
+++ src/main/java/mx/com/nitrostudio/animechannel/models/entities/servers/hosts/OkruServer.kt
@@ -10,7 +10,7 @@ import kotlin.concurrent.thread
 
 class OkruServer : GenericServer(), IServer {
 
-    override fun getName(): String? = "Fire"
+    override fun getName(): String? = "Okru"
 
     override fun isProcessable(): Boolean {
         return true
